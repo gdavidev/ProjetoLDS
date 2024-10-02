4f5fc6179ca1f9a65e63efc8df9d3013cf3f67f7
wolke-leonardo-paixao <leonardo.paixao@wolkedatasolutions.com>
2024-10-02 00:54:17 -0300

    wip: alterado endpoint de download

--- DjangoAPI/roms/serializer.py
+++ DjangoAPI/roms/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import ROM, User, Conversa, ParticipantesCoversa, Mensagem, Postagem, Topico
+from .models import ROM, User, Conversa, ParticipantesCoversa, Mensagem, Postagem, Topico, Emulador
 
 
 #rom serializer
