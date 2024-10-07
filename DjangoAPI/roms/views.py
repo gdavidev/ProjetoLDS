e0ace0bed63b81bb80251fe9b8513f5f5b445d9e
wolke-leonardo-paixao <leonardo.paixao@wolkedatasolutions.com>
2024-10-07 19:21:24 -0300

    refactor: meu deus

--- DjangoAPI/roms/views.py
+++ DjangoAPI/roms/views.py
@@ -73,7 +73,6 @@ class ROMCreate(APIView):
         serializer = ROMSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
