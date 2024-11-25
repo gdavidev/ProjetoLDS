3e5c0e221ab57132cf0b592972d586343283f7f0
David <77506410+gdavidev@users.noreply.github.com>
2024-11-24 23:07:12 -0300

    fix: Fixed Header Dropdown position and behavior

--- src/apps/main/components/layout/Header.tsx
+++ src/apps/main/components/layout/Header.tsx
@@ -6,10 +6,11 @@ import { Link, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import CurrentUser from "@models/CurrentUser";
 import { IonIcon } from "@ionic/react";
-import { caretDown, person } from "ionicons/icons";
+import { caretDown, caretUp, person } from "ionicons/icons";
 import logo from '/icons/logo.png';
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { Button } from "@mui/material";
+import useTailwindTheme from "@/hooks/configuration/useTailwindTheme";
 
 export default function Header() {
   const downloadLink: string = 'https://github.com/Denis-Saavedra/EmuHub-Desktop/raw/refs/heads/main/Instalador/Win32/Debug/EmuHubInstaller.exe'
@@ -29,7 +30,7 @@ export default function Header() {
           </a>
           { 
             user && user.isAuth() ?
-              <LoggedUserDropdown user={ user } logoutFn={ () => setUser(undefined) } /> :
+              <LoggedUserDropdown user={ user } logoutFn={ () => setUser(null) } /> :
               <LoginSigninButtons />
           }
         </div>
@@ -39,31 +40,50 @@ export default function Header() {
   );
 }
 
-function LoggedUserDropdown(props: { user: CurrentUser, logoutFn: () => void }) {  
-  const [ isMenuOpen, setIsMenuOpen ] = useState<boolean>(false);
+function LoggedUserDropdown(props: { user: CurrentUser, logoutFn: () => void }) {
+  const { colors } = useTailwindTheme()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   
   return (
     <div>
-      <Button variant="contained" size="large"
-          startIcon={ <IonIcon icon={ person } /> }
-          endIcon={ <IonIcon icon={ caretDown } /> }
-          onClick={ () => setIsMenuOpen(open => !open) }>
+      <Button 
+        variant="contained" 
+        size="large"
+        startIcon={ <IonIcon icon={ person } /> }
+        endIcon={ <IonIcon icon={ open ? caretUp : caretDown } /> }
+        onClick={ handleClick }
+        sx={{ 
+          backgroundColor: colors['primary'],
+          px: '2rem',
+          py: '0.5rem'
+        }}
+      >
         { props.user.userName }
       </Button>
-      <Menu open={ isMenuOpen }>
-        <MenuItem sx={{padding: '0'}}>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}        
+      >
+        <MenuItem onClick={ handleClose }>
           <Link to="/profile" className="w-full h-full px-8 text-center">Perfil</Link>
         </MenuItem>
-        {
-          // TODO add way to set an admin in the backend
-          true ?
-            <MenuItem sx={{padding: '0'}}>
-              <Link to="/admin/view-games" className="w-full h-full px-8 text-center">Admin</Link>
-            </MenuItem> :
-            ""
-        }
-        <MenuItem sx={{padding: '0'}}>
-          <Link to="/log-in" onClick={ props.logoutFn } className="w-full h-full px-8 text-center">
+        <MenuItem onClick={ handleClose }>
+          <Link to="/admin/view-games" className="w-full h-full px-8 text-center">Admin</Link>
+        </MenuItem>
+        <MenuItem onClick={ handleClose }>
+          <Link 
+            to="/log-in" 
+            onClick={ props.logoutFn } 
+            className="w-full h-full px-8 text-center"
+          >
             Sair
           </Link>        
         </MenuItem>            
