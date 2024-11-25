026f6004ff7fd6a11ef50a88ef5bdec7b340fa56
David <77506410+gdavidev@users.noreply.github.com>
2024-11-25 01:47:39 -0300

    fix: Emulators menu admin layout fixes post material ui version change

--- src/apps/admin/components/modal/EmulatorEditModal.tsx
+++ src/apps/admin/components/modal/EmulatorEditModal.tsx
@@ -6,122 +6,123 @@ import { UseMutationResult } from 'react-query';
 import ModalPopup, { ModalPopupProps } from '@/apps/shared/components/ModalPopup';
 import Emulator from '@/models/Emulator';
 import { FormState, useForm } from 'react-hook-form';
-import { IonIcon } from '@ionic/react';
-import { document, cloudUploadOutline } from 'ionicons/icons';
-import Button from '@mui/material/Button';
 import FileUtil from '@/libs/FileUtil';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import { useStoreEmulator } from '@/hooks/useEmulators';
 
 export type EmulatorEditModalProps = {
-  emulator: Emulator,
-  onChange?: (newEmulator: Emulator) => void
-} & ModalPopupProps
+	emulator: Emulator;
+	onChange?: (newEmulator: Emulator) => void;
+} & ModalPopupProps;
 interface IEmulatorFormData {
-  company: string,
-  console: string,
-  abbreviation: string,
-  file?: FileList,
+	company: string;
+	console: string;
+	abbreviation: string;
+	file?: FileList;
 }
 
 const defaultValues: IEmulatorFormData = {
-  company: "",
-  console: "",
-  abbreviation: "",
-  file: undefined
-}
-
-export default function EmulatorEditModal(props: EmulatorEditModalProps) {
-  const { user } = useCurrentUser();  
-  const { register, handleSubmit, watch, reset: setFormData, clearErrors } = useForm<IEmulatorFormData>({
-    defaultValues: defaultValues,
-  });
-
-  useEffect(() => {
-    if (props.emulator.id !== 0)
-      setFormData({
-        company: props.emulator.companyName,
-        console: props.emulator.console,
-        abbreviation: props.emulator.abbreviation,
-        file: FileUtil.createFileList(props.emulator.file),
-      });
-  }, [props.isOpen]);
-  
-  const { 
-    mutate: storeEmulator,
-    reset: resetStoreEmulator, 
-    isLoading: isSendingEmulator 
-  } = useStoreEmulator(user?.token!, {
-    onSuccess: (emulator: Emulator) => props.onChange?.(emulator),
-    onError: (err: any) => console.log("err: " + JSON.stringify(err)),
-  })
-
-  const resetForm = () => { 
-    resetStoreEmulator();
-    clearErrors();
-    setFormData();
-  }
-
-  function onSubmit(data: IEmulatorFormData) {
-    const emulatorFile: File | undefined = data.file instanceof FileList ? data.file[0] : data.file;
-    const newEmulator: Emulator = new Emulator(
-      props.emulator.id,
-      data.abbreviation,
-      data.console,
-      data.company,
-      emulatorFile,
-    );
-    storeEmulator(newEmulator);
-  }  
-
-  return (
-    <ModalPopup title={ props.emulator.id === 0 ? "Adicionar Jogo" : "Editar Jogo" }
-        isOpen={ props.isOpen } bottomText={ props.bottomText } topText={ props.topText } 
-        onCloseRequest={ () => { resetForm(); props.onCloseRequest?.() } }
-        className="flex flex-col gap-y-3">
-      <form onSubmit={ handleSubmit(onSubmit) }>
-        {/* { getAlert(formState, emulatorService) } */}
-        <div className="flex flex-col gap-y-3 min-h-full justify-between">
-          <FormControl>
-            <FormLabel>Console</FormLabel>
-            <input {...register("console", { required: true })} className='input-text'/>
-          </FormControl>
-          <FormControl>
-            <FormLabel>Empresa</FormLabel>
-            <input {...register("company", { required: true })} className='input-text' />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Abreviação</FormLabel>             
-            <input {...register("abbreviation", { required: true })} className='input-text' />
-          </FormControl>
-          <div className='flex justify-start items-center gap-x-2'>
-            <IonIcon className='min-h-4 min-w-4' icon={ document } />
-            <span className="truncate">{ watch("file")?.[0]?.name }</span>
-          </div>
-          <Button component="label" tabIndex={-1} variant="outlined" startIcon={ <IonIcon icon={ cloudUploadOutline } /> } >
-            Aquivo do Emulador
-            <input type="file" className='hidden' {...register("file", { required: true })} />
-          </Button>
-        </div>
-        <input type="submit" disabled={ isSendingEmulator } value="Confirmar"
-            className={ "btn-r-md bg-primary text-white w-full mt-4" + (isSendingEmulator ? " disabled" : "") } />
-      </form>
-    </ModalPopup>
-  );
+	company: '',
+	console: '',
+	abbreviation: '',
+	file: undefined,
 };
 
-function getAlert(formState: FormState<IEmulatorFormData>, emulatorService: UseMutationResult<any, any, any, any>): JSX.Element | undefined {
-  if (formState.errors.console)
-    return <Alert color="error" >Campo console vazío.</Alert>
-  if (formState.errors.company)
-    return <Alert color="error" >Campo empresa vazío.</Alert>
-  if (formState.errors.abbreviation)
-    return <Alert color="error" >Campo nome vazío.</Alert>
-  if (emulatorService.isLoading)
-    return <Alert color="warning" >Enviando...</Alert>
-  if (emulatorService.isSuccess)
-    return <Alert color="success" >Enviado com sucesso!</Alert>
-  if (emulatorService.isError)
-    return <Alert color="error" >{ emulatorService.error.message }</Alert>
-  return undefined
+export default function EmulatorEditModal(props: EmulatorEditModalProps) {
+	const { user } = useCurrentUser();
+	const {
+		register,
+		handleSubmit,
+		reset: setFormData,
+		clearErrors,
+	} = useForm<IEmulatorFormData>({
+		defaultValues: defaultValues,
+	});
+
+	useEffect(() => {
+		if (props.emulator.id !== 0)
+			setFormData({
+				company: props.emulator.companyName,
+				console: props.emulator.console,
+				abbreviation: props.emulator.abbreviation,
+				file: FileUtil.createFileList(props.emulator.file),
+			});
+	}, [props.isOpen]);
+
+	const {
+		mutate: storeEmulator,
+		reset: resetStoreEmulator,
+		isLoading: isSendingEmulator,
+	} = useStoreEmulator(user?.token!, {
+		onSuccess: (emulator: Emulator) => props.onChange?.(emulator),
+		onError: (err: any) => console.log('err: ' + JSON.stringify(err)),
+	});
+
+	const resetForm = () => {
+		resetStoreEmulator();
+		clearErrors();
+		setFormData();
+	};
+
+	function onSubmit(data: IEmulatorFormData) {
+		const emulatorFile: File | undefined = data.file instanceof FileList ? data.file[0] : data.file;
+		const newEmulator: Emulator = new Emulator(
+			props.emulator.id,
+			data.abbreviation,
+			data.console,
+			data.company,
+			emulatorFile
+		);
+		storeEmulator(newEmulator);
+	}
+
+	return (
+		<ModalPopup
+			title={props.emulator.id === 0 ? 'Adicionar Jogo' : 'Editar Jogo'}
+			isOpen={props.isOpen}
+			bottomText={props.bottomText}
+			topText={props.topText}
+			onCloseRequest={() => {
+				resetForm();
+				props.onCloseRequest?.();
+			}}
+			className='flex flex-col gap-y-3'>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				{/* { getAlert(formState, emulatorService) } */}
+				<div className='flex min-h-full flex-col justify-between gap-y-3'>
+					<FormControl>
+						<FormLabel>Console</FormLabel>
+						<input {...register('console', { required: true })} className='input-text' />
+					</FormControl>
+					<FormControl>
+						<FormLabel>Empresa</FormLabel>
+						<input {...register('company', { required: true })} className='input-text' />
+					</FormControl>
+					<FormControl>
+						<FormLabel>Abreviação</FormLabel>
+						<input {...register('abbreviation', { required: true })} className='input-text' />
+					</FormControl>
+				</div>
+				<input
+					type='submit'
+					disabled={isSendingEmulator}
+					value='Confirmar'
+					className={'btn-r-md mt-4 w-full bg-primary text-white' + (isSendingEmulator ? ' disabled' : '')}
+				/>
+			</form>
+		</ModalPopup>
+	);
+}
+
+function getAlert(
+	formState: FormState<IEmulatorFormData>,
+	emulatorService: UseMutationResult<any, any, any, any>
+): JSX.Element | undefined {
+	if (formState.errors.console) return <Alert color='error'>Campo console vazío.</Alert>;
+	if (formState.errors.company) return <Alert color='error'>Campo empresa vazío.</Alert>;
+	if (formState.errors.abbreviation) return <Alert color='error'>Campo nome vazío.</Alert>;
+	if (emulatorService.isLoading) return <Alert color='warning'>Enviando...</Alert>;
+	if (emulatorService.isSuccess) return <Alert color='success'>Enviado com sucesso!</Alert>;
+	if (emulatorService.isError) return <Alert color='error'>{emulatorService.error.message}</Alert>;
+	return undefined;
 }
--- src/apps/admin/pages/EmulatorsView.tsx
+++ src/apps/admin/pages/EmulatorsView.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import { IonIcon } from '@ionic/react'
+import { IonIcon } from '@ionic/react';
 import { add, createOutline, trashOutline, reloadOutline } from 'ionicons/icons';
 import TableDisplay from '@apps/admin/components/TableDisplay';
 import { IconButton } from '@mui/material';
@@ -12,118 +12,129 @@ import useEmulators, { useDeleteEmulator } from '@/hooks/useEmulators';
 import useCurrentUser from '@/hooks/useCurrentUser';
 
 export default function EmulatorsView() {
-  const [ emulatorModalData  , setEmulatorModalData   ] = useState<Emulator>(new Emulator());
-  const [ isEmulatorModalOpen, setIsEmulatorModalOpen ] = useState<boolean>(false);
-  const { user } = useCurrentUser();
-  const emulators = useStatefulArray<Emulator>([], { 
-    compare: (emu1: Emulator, emu2: Emulator) => emu1.id === emu2.id
-  });
+	const [emulatorModalData, setEmulatorModalData] = useState<Emulator>(new Emulator());
+	const [isEmulatorModalOpen, setIsEmulatorModalOpen] = useState<boolean>(false);
+	const { user } = useCurrentUser();
+	const emulators = useStatefulArray<Emulator>([], {
+		compare: (emu1: Emulator, emu2: Emulator) => emu1.id === emu2.id,
+	});
 
-  const { refetch: reload } = useEmulators({
-    onSuccess: (list: Emulator[]) => emulators.set(list.sort((prev, curr) => prev.id - curr.id)),
-    onError: (err: any) => console.log("err: " + JSON.stringify(err))
-  });
-  const { mutate: deleteEmulator } = useDeleteEmulator(user?.token!, {
-    onSuccess: (emulator: Emulator) => emulators.remove(emulator),
-    onError: (err: any) => console.log("err: " + JSON.stringify(err))
-  });
+	const { refetch: reload } = useEmulators({
+		onSuccess: (list: Emulator[]) => emulators.set(list.sort((prev, curr) => prev.id - curr.id)),
+		onError: (err: any) => console.log('err: ' + JSON.stringify(err)),
+	});
+	const { mutate: deleteEmulator } = useDeleteEmulator(user?.token!, {
+		onSuccess: (emulator: Emulator) => emulators.remove(emulator),
+		onError: (err: any) => console.log('err: ' + JSON.stringify(err)),
+	});
 
-  function addEmulator() {
-    setEmulatorModalData(new Emulator());
-    setIsEmulatorModalOpen(true);
-  }
-  function editEmulator(emulator: Emulator) {
-    setEmulatorModalData(emulator);
-    setIsEmulatorModalOpen(true);
-  }
+	function addEmulator() {
+		setEmulatorModalData(new Emulator());
+		setIsEmulatorModalOpen(true);
+	}
+	function editEmulator(emulator: Emulator) {
+		setEmulatorModalData(emulator);
+		setIsEmulatorModalOpen(true);
+	}
 
-  const templateHeader: {colName: string, colWidth: string}[] = [
-    {colName: '#'          , colWidth: '30px'  },
-    {colName: 'Console'    , colWidth: '100%'  },
-    {colName: 'Empresa'    , colWidth: '120px' },
-    {colName: 'Abreviação' , colWidth: '160px' }, 
-    {colName: ''           , colWidth: '120px' }
-  ];
+	const templateHeader: { colName: string; colWidth: string }[] = [
+		{ colName: '#', colWidth: '30px' },
+		{ colName: 'Console', colWidth: '100%' },
+		{ colName: 'Empresa', colWidth: '120px' },
+		{ colName: 'Abreviação', colWidth: '160px' },
+		{ colName: '', colWidth: '120px' },
+	];
 
-  return (
-    <>
-      <div className="flex flex-col">
-        <div className="flex justify-between items-center mx-5 text-white">
-          <h2 className="font-rubik font-bold">Lista de Jogos</h2>
-          <div className="flex gap-x-2 ">
-            <IconButton onClick={ () => reload() } >
-              <IonIcon icon={ reloadOutline } />
-            </IconButton>
-            <button className='btn-r-md bg-primary hover:bg-primary-dark text-white'
-                onClick={ addEmulator }>
-              <IonIcon icon={ add } /> Novo Jogo
-            </button>
-          </div>
-        </div>
-        <TableDisplay headerTemplateLabels={ templateHeader } 
-            tableStyleObject={{width: '100%', borderSpacing: '0 3px'}}
-            tableHeaderClassName='text-white font-rubik font-bold'>
-          { 
-            emulators.all.map((emulator: Emulator, index: number) => {
-                return ( 
-                  <EmulatorDataTableRow key={index} emulator={ emulator }
-                    rowClassName="bg-primary-light text-white"
-                    cellClassName='first:rounded-s-md last:rounded-e-md'
-                    actions={{
-                      edit: editEmulator, 
-                      delete: deleteEmulator
-                    }} />
-                )
-              })            
-          }
-        </TableDisplay>
-      </div>
-      <EmulatorEditModal emulator={ emulatorModalData }
-          onCloseRequest={ () => { setIsEmulatorModalOpen(false) } } isOpen={ isEmulatorModalOpen } 
-          onChange={ emulatorModalData.id === 0 ? 
-            emulators.append :
-            emulators.update } />
-    </>
-  );
+	return (
+		<>
+			<div className='flex flex-col'>
+				<div className='mx-5 flex items-center justify-between text-white'>
+					<h2 className='font-rubik font-bold'>Lista de Jogos</h2>
+					<div className='flex gap-x-2'>
+						<IconButton onClick={() => reload()}>
+							<IonIcon icon={reloadOutline} />
+						</IconButton>
+						<button className='btn-r-md bg-primary text-white hover:bg-primary-dark' onClick={addEmulator}>
+							<IonIcon icon={add} /> Novo Jogo
+						</button>
+					</div>
+				</div>
+				<TableDisplay
+					headerTemplate={templateHeader}
+					tableStyleObject={{
+						width: '100%',
+						borderSpacing: '0 3px',
+					}}
+					tableHeaderClassName='text-white font-rubik font-bold'
+				>
+					{emulators.all.map((emulator: Emulator, index: number) => {
+						return (
+							<EmulatorDataTableRow
+								key={index}
+								emulator={emulator}
+								rowClassName='bg-primary-light text-white'
+								cellClassName='first:rounded-s-md last:rounded-e-md'
+								actions={{
+									edit: editEmulator,
+									delete: deleteEmulator,
+								}}
+							/>
+						);
+					})}
+				</TableDisplay>
+			</div>
+			<EmulatorEditModal
+				emulator={emulatorModalData}
+				onCloseRequest={() => {
+					setIsEmulatorModalOpen(false);
+				}}
+				isOpen={isEmulatorModalOpen}
+				onChange={emulatorModalData.id === 0 ? emulators.append : emulators.update}
+			/>
+		</>
+	);
 }
 
 type EmulatorDataTableRowProps = {
-  rowClassName?: string,
-  cellClassName?: string,
-  emulator: Emulator,
-  actions: { 
-    edit: (emulator: Emulator) => void,
-    delete: (emulator: Emulator) => void,
-  }
+	rowClassName?: string;
+	cellClassName?: string;
+	emulator: Emulator;
+	actions: {
+		edit: (emulator: Emulator) => void;
+		delete: (emulator: Emulator) => void;
+	};
+};
+function EmulatorDataTableRow(props: EmulatorDataTableRowProps): React.ReactElement {
+	return (
+		<tr className={props.rowClassName}>
+			<td className={props.cellClassName}>{props.emulator.id}</td>
+			<td className={props.cellClassName}>{props.emulator.console}</td>
+			<td className={props.cellClassName}>{props.emulator.companyName}</td>
+			<td className={props.cellClassName}>{props.emulator.abbreviation}</td>
+			<td className={props.cellClassName}>
+				<Box sx={{ display: 'flex', gap: 1 }}>
+					<IconButton
+						size='small'
+						color='default'
+						onClick={() => {
+							props.actions.edit(props.emulator);
+						}}
+						sx={{ p: '10px' }}
+					>
+						<IonIcon style={{ color: '#FFFFFF' }} icon={createOutline} />
+					</IconButton>
+					<IconButton
+						size='small'
+						color='default'
+						onClick={() => {
+							props.actions.delete(props.emulator);
+						}}
+						sx={{ p: '10px' }}
+					>
+						<IonIcon style={{ color: '#FFFFFF' }} icon={trashOutline} />
+					</IconButton>
+				</Box>
+			</td>
+		</tr>
+	);
 }
-function EmulatorDataTableRow(props: EmulatorDataTableRowProps): React.ReactElement {  
-  return (
-    <tr className={ props.rowClassName }>
-      <td className={ props.cellClassName }>
-        { props.emulator.id   }
-      </td>
-      <td className={ props.cellClassName }>
-        { props.emulator.console }
-      </td>
-      <td className={ props.cellClassName }>
-        { props.emulator.companyName }
-      </td>
-      <td className={ props.cellClassName }>
-        { props.emulator.abbreviation }
-      </td>
-      <td className={ props.cellClassName }>
-        <Box sx={{ display: 'flex', gap: 1 }}>
-          <Button size="small" variant="contained" 
-              onClick={ () => { props.actions.edit(props.emulator) } } >
-            <IonIcon style={{color: '#FFFFFF'}} icon={ createOutline } />
-          </Button>
-          <Button size="small" variant="contained"
-             onClick={ () => { props.actions.delete(props.emulator) } } >
-            <IonIcon style={{color: '#FFFFFF'}} icon={ trashOutline } />
-          </Button>
-        </Box>
-      </td>
-    </tr>
-  )
-}
-
