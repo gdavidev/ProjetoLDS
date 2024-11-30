76a834e20f503cb6aacaefad8d21df668eea871b
David <77506410+gdavidev@users.noreply.github.com>
2024-11-30 10:52:48 -0300

    fix: GamesPage.tsx Sidebar styled and working

--- src/apps/main/components/layout/SearchGamesSideBar.tsx
+++ src/apps/main/components/layout/SearchGamesSideBar.tsx
@@ -1,45 +1,67 @@
-import EmulatorApiService from "@/api/EmulatorApiService";
-import CheckBox from "@apps/shared/components/formComponents/CheckBox";
-import Emulator from "@models/Emulator";
-import { Accordion, AccordionDetails, AccordionSummary, Checkbox } from "@mui/material";
-import { useEffect, useState } from "react";
+import CheckBox from '@apps/shared/components/formComponents/CheckBox';
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
+import { IonIcon } from '@ionic/react';
+import { chevronForwardOutline, filterOutline } from 'ionicons/icons';
+import { AccordionProps } from '@mui/material/Accordion';
+import { accordionSummaryClasses, AccordionSummaryProps } from '@mui/material/AccordionSummary';
+import useCategories, { CategoryType } from '@/hooks/useCategories.ts';
+import useEmulators from '@/hooks/useEmulators.ts';
+
+const AccordionStyled = (props: AccordionProps) => (
+  <Accordion {...props}
+    disableGutters
+    sx={{
+      backgroundColor: 'transparent',
+      border: 'none',
+      color: 'white',
+      boxShadow: 'none',
+    }} />
+);
+const AccordionSummaryStyled = (props: AccordionSummaryProps) => (
+    <AccordionSummary {...props}
+      expandIcon={ <IonIcon style={{color: 'white'}} icon={ chevronForwardOutline } /> }
+      sx={{
+        display: 'flex',
+        flexDirection: 'row-reverse',
+        columnGap: '0.5rem',
+        [`& .${accordionSummaryClasses.expandIconWrapper}.${accordionSummaryClasses.expanded}`]: {
+          transform: 'rotate(90deg)',
+        }
+      }} />
+);
 
 export default function SearchGamesSideBar() {
-  const [ emulatorList, setEmulatorList ] = useState<Emulator[]>([]);
-
-  useEffect(() => {
-    fetchEmulatorData();
-  }, [])
-
-  function fetchEmulatorData() {
-    EmulatorApiService.getAll()
-      .then(data => {        
-        data.sort((prev: Emulator, curr: Emulator) => prev.id - curr.id)
-        setEmulatorList(data);
-      });
-  }
+  const { data: categories } = useCategories(CategoryType.GAMES);
+  const { data: emulators  } = useEmulators();
 
   return (
-    <aside className="min-w-56 ps-6 pe-3">
-      <Accordion>
-        <AccordionSummary>Emulador</AccordionSummary>
+    <aside className="min-w-72 flex flex-col">
+      <AccordionStyled>
+        <AccordionSummaryStyled>Emulador</AccordionSummaryStyled>
         <AccordionDetails>
-          { emulatorList.map((em, i) => 
+          { emulators && emulators.map((em, i) =>
               <CheckBox key={i} 
                   className=""
                   name={ em.console } 
                   label={ em.console } />
           ) }
         </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary>Title</AccordionSummary>
+      </AccordionStyled>
+      <AccordionStyled>
+        <AccordionSummaryStyled>Categorias</AccordionSummaryStyled>
         <AccordionDetails>
-          <Checkbox />
-          <Checkbox />
-          <Checkbox />
+          { categories && categories.map((cat, i) =>
+              <CheckBox key={i}
+                  className=""
+                  name={ cat.name }
+                  label={ cat.name } />
+          ) }
         </AccordionDetails>
-      </Accordion>
+      </AccordionStyled>
+      <button className='btn-r-md bg-primary text-white gap-x-2 mt-2 self-end'>
+        <IonIcon style={{color: 'white'}} icon={ filterOutline } />
+        Filtrar
+      </button>
     </aside>
   )
 }
