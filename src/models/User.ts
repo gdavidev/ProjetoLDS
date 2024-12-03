981acfcebbb9bed1a26f77e6ca71eee3ceaeee85
David <77506410+gdavidev@users.noreply.github.com>
2024-12-03 11:22:39 -0300

    feat: Added Thumbnail to User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,13 @@
+import Thumbnail from '@models/utility/Thumbnail.ts';
+
 export default class User {
   id: number
   name: string
+  image: Thumbnail | null;
 
-  constructor(id?: number, name?: string) {
-    this.id   = id   || 0;
-    this.name = name || '';
+  constructor(id?: number, name?: string, image?: string) {
+    this.id     = id      || 0;
+    this.name   = name    || '';
+    this.image  = image ? new Thumbnail(image) : null;
   }
 }
