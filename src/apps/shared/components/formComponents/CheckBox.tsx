dc79f81a44d1b88bd92cd7b7863c24d3e7399ed0
David <77506410+gdavidev@users.noreply.github.com>
2024-12-03 21:42:41 -0300

    fix: CheckBox.tsx not changing when clicking directly on the checkbox

--- src/apps/shared/components/formComponents/CheckBox.tsx
+++ src/apps/shared/components/formComponents/CheckBox.tsx
@@ -11,13 +11,24 @@ export default function CheckBox(props: CheckBoxProps) {
   const [ isChecked, setIsChecked ] = useState(props.isChecked);
 
   return (
-    <div className={ "flex items-center cursor-pointer " + (props.className ?? '') }
-        onClick={ (e) => { setIsChecked(!isChecked); e.preventDefault() } }>
+    <div
+      className={ "flex items-center cursor-pointer " + (props.className ?? '') }
+      onClick={ (e) => {
+          setIsChecked(!isChecked);
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      }>
       <input type="checkbox" 
-          className="w-5 h-5 me-2"
-          id={ props.name } 
-          name={ props.name } 
-          checked={ isChecked } />      
+        className="w-5 h-5 me-2"
+        id={ props.name }
+        name={ props.name }
+        checked={ isChecked }
+        onClick={ (e) => {
+          e.stopPropagation();
+          setIsChecked(ck => !ck);
+        }
+      }/>
       <label htmlFor={ props.name }
           className="cursor-pointer">
         { props.label }
