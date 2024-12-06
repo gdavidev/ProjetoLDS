429919db7c62c82c20767c82a42e1772741993d4
David <77506410+gdavidev@users.noreply.github.com>
2024-12-05 23:41:18 -0300

    refact/fix: CheckBox.tsx excess code cleaned and pointer bug fixed

--- src/apps/shared/components/formComponents/CheckBox.tsx
+++ src/apps/shared/components/formComponents/CheckBox.tsx
@@ -12,21 +12,14 @@ export default function CheckBox(props: CheckBoxProps) {
 
   return (
     <div
-      className={ "flex items-center cursor-pointer " + (props.className ?? '') }
-      onClick={ (e) => {
-          setIsChecked(!isChecked);
-          e.stopPropagation();
-        }
-      }>
+        className={ "flex items-center " + (props.className ?? '') }
+        onClick={ () => setIsChecked(c => !c) }>
       <input type="checkbox" 
-        className="w-5 h-5 me-2"
-        id={ props.name }
-        name={ props.name }
-        defaultChecked={ isChecked }
-        onClick={ (e) => {
-          e.stopPropagation();
-        }
-      }/>
+          className="w-5 h-5 me-2 cursor-pointer"
+          id={ props.name }
+          name={ props.name }
+          defaultChecked={ isChecked }
+          onClick={ (e) => e.stopPropagation() }/>
       <label htmlFor={ props.name }
           className="cursor-pointer">
         { props.label }
