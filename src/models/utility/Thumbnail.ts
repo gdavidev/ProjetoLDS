e13d32d8af6a08e0dae000791d2d9b7963da0284
David <77506410+gdavidev@users.noreply.github.com>
2024-12-14 23:51:18 -0300

    fix: Updated FileUtil.ts and Thumbnail.ts to pick right base64 prefix to fix display issues

--- src/models/utility/Thumbnail.ts
+++ src/models/utility/Thumbnail.ts
@@ -11,14 +11,14 @@ export default class Thumbnail {
       throw new Error("No source provided");
 
     this.file = source.file || null;
-    this.base64 = source.base64 || null;
+    this.base64 = source.base64 ? this.getPrefixedBase64Image(source.base64) : null;
     this.url = source.url || null;
     this.fallbackUrl = source.fallbackUrl || null;
   }
 
   toDisplayable(): string {
     if (this.base64 !== null)
-      return 'data:image/jpeg;base64,' + this.base64;
+      return this.getPrefixedBase64Image(this.base64);
     if (this.url !== null)
       return this.url;
     if (this.file) {
@@ -37,5 +37,20 @@ export default class Thumbnail {
     else if (this.file !== null)
       return await FileUtil.fileToBase64(this.file)
     return null;
+  }
+
+  private getPrefixedBase64Image(source: string) {
+    const sourceAlreadyHasPrefix = source.indexOf('data:image');
+    if (sourceAlreadyHasPrefix >= 0)
+      return source;
+
+    switch(true) {
+      case source.startsWith('/9j/'): return 'data:image/jpeg;base64,' + source;
+      case source.startsWith('iVBORw'): return 'data:image/png;base64,' + source;
+      case source.startsWith('UklGR'): return 'data:image/webp;base64,' + source;
+      case source.startsWith('AAABAA'): return 'data:image/x-icon;base64,' + source;
+      case source.startsWith('R0lGOD'): return 'data:image/gif;base64,' + source;
+      default: return 'data:image/jpeg;base64,' + source;
+    }
   }
 }
