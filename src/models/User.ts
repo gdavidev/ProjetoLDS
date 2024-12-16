b6294fe6061f13e69fcfecd89c126b8d58c25d13
David <77506410+gdavidev@users.noreply.github.com>
2024-12-16 15:27:56 -0300

    fix: Fixed fallback image when creating a User object

--- src/models/User.ts
+++ src/models/User.ts
@@ -10,7 +10,12 @@ export default class User {
   constructor(id: number, name: string, profilePic?: Thumbnail) {
     this.id         = id;
     this.name       = name;
-    this.profilePic = profilePic || new Thumbnail({ fallbackUrl: userImageNotFound });
+    if (profilePic) {
+      profilePic.fallbackUrl = userImageNotFound;
+      this.profilePic = profilePic;
+    } else {
+      this.profilePic = new Thumbnail({ fallbackUrl: userImageNotFound });
+    }
   }
 
   static fromCurrentUser(user: CurrentUser) {
